3361db17ed5fb4f195dd3fd42fb10ac6d38b84bb
soumenh <61101884+soumenh@users.noreply.github.com>
2020-11-16 22:39:39 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,9 +1,11 @@
 clear
 echo -e "𝚂𝚃𝙰𝚁𝚃𝙸𝙽𝙶 𝙿𝙻𝙴𝙰𝚂𝙴 𝚆𝙰𝙸𝚃...." |lolcat -a -d 20
 clear
+echo -e "\e[91m "
 echo -e "\n    𝙽𝙾𝚆 𝚃𝙷𝙴 𝚃𝙸𝙼𝙴 𝙸𝚂:  " | lolcat | pv -qL 120
 date
 echo -e "\n    𝙼𝙾𝙱𝙸𝙻𝙴 𝙳𝙴𝚃𝙰𝙸𝙻𝚂:  " | lolcat | pv -qL 120
+echo -e "\e[92m "
 echo -e -n "\n𝙶𝚂𝙼 𝙾𝚙𝚎𝚛𝚊𝚝𝚘𝚛: " | lolcat | pv -qL 250
 getprop gsm.operator.alpha
 sleep 0.40
@@ -36,6 +38,7 @@ echo " 𝚁𝙰𝙼 : " $RAM | lolcat | pv -qL 250
 sleep 1
 echo
 clear
+echo -e "\e[94m "
 echo -e "  𝚌𝚘𝚗𝚗𝚎𝚌𝚝𝚒𝚗𝚐 𝚠𝚒𝚝𝚑 𝚜𝚎𝚛𝚟𝚎𝚛 𝚙𝚕𝚎𝚊𝚜𝚎 𝚠𝚊𝚒𝚝• " 
 sleep 0.45
 clear
@@ -63,38 +66,47 @@ clear
 echo -e "  𝚌𝚘𝚗𝚗𝚎𝚌𝚝𝚒𝚗𝚐 𝚠𝚒𝚝𝚑 𝚜𝚎𝚛𝚟𝚎𝚛 𝚙𝚕𝚎𝚊𝚜𝚎 𝚠𝚊𝚒𝚝• " 
 sleep 0.45
 clear
-
+echo -e "\e[92m "
 echo -e "𝚂𝙴𝚁𝚅𝙴𝚁 𝙲𝙾𝙽𝙽𝙴𝙲𝚃𝙴𝙳 , 𝙻𝙴𝚃𝚂 𝙴𝙽𝙹𝙾𝚈" | lolcat | pv -qL 220
 sleep 1
 
 
 clear && echo " " && echo " "
 if [ -d /data/data/com.duoqshsqx ]; then
+echo -e "\e[92m "
 echo -e "[𝙺𝚁𝙰𝚃𝙾𝚂𝙴𝚇𝙲𝙴𝙻𝙴𝙽𝚃 𝙶𝙶]         𝙵𝙾𝚄𝙽𝙳!" | lolcat | pv -qL 120
 else
+echo -e "\e[91m "
     echo -e "[𝙺𝚁𝙰𝚃𝙾𝚂𝙴𝚇𝙲𝙴𝙻𝙴𝙽𝚃 𝙶𝙶 ] ➟   𝚗𝚘𝚝 𝚏𝚘𝚞𝚗𝚍, 𝚢𝚘𝚞 𝚊𝚛𝚎 𝚗𝚘𝚝 𝚞𝚜𝚒𝚗𝚐 𝙺𝚁𝙰𝚃𝙾𝚂𝙴𝚇𝙲𝙴𝙻𝙴𝙽𝚃 𝙶𝙶" | lolcat | pv -qL 120
    echo -e ""
  exit 0
 fi
 if [ -d /data/data/com.tencent.mobileqqsa ]; then
+echo -e "\e[92m "
 echo -e "[𝙺𝙴 𝚅𝙰𝚁𝚃𝚄𝙰𝙻]    𝙵𝙾𝚄𝙽𝙳 !" | lolcat | pv -qL 120
 else
+echo -e "\e[91m "
     echo -e "[𝙺𝙴 𝚅𝙰𝚁𝚃𝚄𝙰𝙻 ] ➟  𝚗𝚘𝚝 𝚏𝚘𝚞𝚗𝚍, 𝚢𝚘𝚞 𝚊𝚛𝚎 𝚗𝚘𝚝 𝚞𝚜𝚒𝚗𝚐 𝙺𝙴 𝚅𝙰𝚁𝚃𝚄𝙰𝙻 " | lolcat | pv -qL 120
     echo -e ""
 exit 0
 fi
 if ls /sdcard/*/*.lua> /dev/null 2>&1; then
+echo -e "\e[92m "
     echo -e "[𝙺𝙴 𝙱𝚈𝙿𝙰𝚂𝚂  𝚂𝙲𝚁𝙸𝙿𝚃]        𝙵𝙾𝚄𝙽𝙳!" | lolcat | pv -qL 120
 else
 if ls /sdcard/*.lua> /dev/null 2>&1; then
+echo -e "\e[92m "
     echo -e "[𝙺𝙴 𝙱𝚈𝙿𝙰𝚂𝚂  𝚂𝙲𝚁𝙸𝙿𝚃        𝙵𝙾𝚄𝙽𝙳!" | lolcat | pv -qL 120
  else
  if ls /sdcard/*/*/*.lua> /dev/null 2>&1; then
+echo -e "\e[92m "
     echo -e "[𝙺𝙴 𝙱𝚈𝙿𝙰𝚂𝚂  𝚂𝙲𝚁𝙸𝙿𝚃]        𝙵𝙾𝚄𝙽𝙳!" | lolcat | pv -qL 120
  else
  if ls /sdcard/*/*/*/*.lua> /dev/null 2>&1; then
+echo -e "\e[92m "
     echo -e "[𝙺𝙴 𝙱𝚈𝙿𝙰𝚂𝚂  𝚂𝙲𝚁𝙸𝙿𝚃]        𝙵𝙾𝚄𝙽𝙳!" | lolcat | pv -qL 120
  else
+echo -e "\e[91m "
     echo -e "[𝙺𝙴 𝙱𝚈𝙿𝙰𝚂𝚂  𝚂𝙲𝚁𝙸𝙿𝚃 ] ➟   𝚗𝚘𝚝 𝚏𝚘𝚞𝚗𝚍, 𝚢𝚘𝚞 𝚊𝚛𝚎 𝚗𝚘𝚝 𝚞𝚜𝚒𝚗𝚐 𝙺𝙴 𝙱𝚈𝙿𝙰𝚂𝚂  𝚂𝙲𝚁𝙸𝙿𝚃" | lolcat | pv -qL 120
     echo -e ""
 exit 0
@@ -105,17 +117,22 @@ fi
 
 
 if ls /sdcard/*/*.lua> /dev/null 2>&1; then
+echo -e "\e[92m "
     echo -e "[𝙷𝙰𝙲𝙺 𝚂𝙲𝚁𝙸𝙿𝚃]          𝙵𝙾𝚄𝙽𝙳!" | lolcat | pv -qL 120
 else
      if ls /sdcard/*.lua> /dev/null 2>&1; then
+echo -e "\e[92m "
     echo -e "[𝙷𝙰𝙲𝙺 𝚂𝙲𝚁𝙸𝙿𝚃]        𝙵𝙾𝚄𝙽𝙳!" | lolcat | pv -qL 120
  else
  if ls /sdcard/*/*/*.lua> /dev/null 2>&1; then
+echo -e "\e[92m "
     echo -e "[𝙷𝙰𝙲𝙺 𝚂𝙲𝚁𝙸𝙿𝚃]        𝙵𝙾𝚄𝙽𝙳!" | lolcat | pv -qL 120
  else
  if ls /sdcard/*/*/*/*.lua> /dev/null 2>&1; then
+echo -e "\e[92m "
     echo -e "[𝙷𝙰𝙲𝙺 𝚂𝙲𝚁𝙸𝙿𝚃]        𝙵𝙾𝚄𝙽𝙳!" | lolcat | pv -qL 120
  else
+echo -e "\e[91m "
     echo -e "[𝙷𝙰𝙲𝙺 𝚂𝙲𝚁𝙸𝙿𝚃 ] ➟  𝚗𝚘𝚝 𝚏𝚘𝚞𝚗𝚍, 𝚢𝚘𝚞 𝚊𝚛𝚎 𝚗𝚘𝚝 𝚞𝚜𝚒𝚗𝚐  𝙺𝙴 𝚅𝙸𝙿 𝙷𝙰𝙲𝙺 𝚂𝙲𝚁𝙸𝙿𝚃!" | lolcat | pv -qL 120
     echo -e ""
 exit 0
@@ -125,7 +142,8 @@ fi
 fi
    echo
 clear
-echo "                          𝐇𝐀𝐂𝐊 𝐃𝐀𝐓𝐀 𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃𝐈𝐍𝐆...            "
+echo -e "\e[95m "
+echo "                 𝐇𝐀𝐂𝐊 𝐃𝐀𝐓𝐀 𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃𝐈𝐍𝐆...            "
 echo -n "  █" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" | lolcat | pv -qL 120
  if [ -d /storage/emulated/0/KRATOSEXCELENT ]; then
 rm -rf /storage/emulated/0/KRATOSEXCELENT &> /dev/null 
@@ -135,6 +153,8 @@ mkidr /storage/emulated/0/KRATOSEXCELENT &> /dev/null
 wget -O /sdcard/KRATOSEXCELENT/core_patch_1.1.0.14555.pak https://drive.google.com/file/d/1cWCLyrIUX7noxU94XpU0bF3bYNScNEpP/view?usp=drivesdk &> /dev/null 
 wget -O /sdcard/KRATOSEXCELENT/game_patch_1.1.0.14556.pak https://drive.google.com/file/d/1cYfyu1y6_XJWDjNUS9SFN4-Shvv7FAM3/view?usp=drivesdk &> /dev/null 
 wget -O /sdcard/KRATOSEXCELENT/game_patch_1.1.0.14557.pak https://drive.google.com/file/d/1cYQ7hSnYi27x_3obFasMPmwKJRCMZdxW/view?usp=drivesdk &> /dev/null 
+echo " "
+echo -e "\e[91m "
 echo -e " 𝙳𝙰𝚃𝙰 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳 𝙲𝙾𝙼𝙿𝙻𝙴𝚃𝙴 " | lolcat | pv -qL 120
    fi
    
