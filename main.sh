e3d42ac91eb0160106b83bedabb342d4adeef102
soumenh <61101884+soumenh@users.noreply.github.com>
2020-11-16 13:55:48 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,155 @@
-echo "it's main manu"
+clear
+echo -e "𝚂𝚃𝙰𝚁𝚃𝙸𝙽𝙶 𝙿𝙻𝙴𝙰𝚂𝙴 𝚆𝙰𝙸𝚃...." |lolcat -a -d 20
+clear
+echo -e "\n    𝙽𝙾𝚆 𝚃𝙷𝙴 𝚃𝙸𝙼𝙴 𝙸𝚂:  " | lolcat | pv -qL 120
+date
+echo -e "\n    𝙼𝙾𝙱𝙸𝙻𝙴 𝙳𝙴𝚃𝙰𝙸𝙻𝚂:  " | lolcat | pv -qL 120
+echo -e -n "\n𝙶𝚂𝙼 𝙾𝚙𝚎𝚛𝚊𝚝𝚘𝚛: " | lolcat | pv -qL 250
+getprop gsm.operator.alpha
+sleep 0.40
+echo -e -n "\n𝙳𝚛𝚘𝚒𝚍 𝙱𝚛𝚊𝚗𝚍: " | lolcat | pv -qL 250
+getprop ro.product.brand
+sleep 0.40
+echo -e -n "\n𝙳𝚛𝚘𝚒𝚍 𝙼𝚘𝚍𝚎𝚕: " | lolcat | pv -qL 250
+getprop ro.product.model
+sleep 0.40
+echo -e -n "\n𝙰𝙿𝙸 𝙻𝚎𝚟𝚎𝚕: " | lolcat | pv -qL 250
+getprop ro.product.first_api_level
+sleep 0.40
+echo -e -n "\n𝚂𝙳𝙺 𝙸𝚗𝚏𝚘: " | lolcat | pv -qL 250
+getprop ro.build.version.sdk
+sleep 0.40
+echo -e -n "\n𝙲𝙿𝚄 𝙰𝙱𝙸: " | lolcat | pv -qL 250
+getprop ro.product.cpu.abi
+sleep 0.40
+echo -e -n "\n𝚃𝚒𝚖𝚎 𝚉𝚘𝚗𝚎: " | lolcat | pv -qL 250
+getprop persist.sys.timezone
+sleep 0.40
+echo -e -n "\n𝙳𝚎𝚟𝚒𝚌𝚎 𝙷𝚊𝚛𝚍𝚠𝚊𝚛𝚎: " | lolcat | pv -qL 250
+getprop ro.hardware
+sleep 0.40
+echo -e -n "\n𝙼𝚎𝚖𝚘𝚛𝚢 𝙸𝚗𝚏𝚘: " | lolcat | pv -qL 250
+echo -e ""
+RAM=`grep "MemTotal" /proc/meminfo | cut -f 2 -d ':'`
+sleep 0.40
+echo " 𝚁𝙰𝙼 : " $RAM | lolcat | pv -qL 250
+sleep 1
+echo
+clear
+echo -e "  𝚌𝚘𝚗𝚗𝚎𝚌𝚝𝚒𝚗𝚐 𝚠𝚒𝚝𝚑 𝚜𝚎𝚛𝚟𝚎𝚛 𝚙𝚕𝚎𝚊𝚜𝚎 𝚠𝚊𝚒𝚝• " 
+sleep 0.45
+clear
+echo -e "  𝚌𝚘𝚗𝚗𝚎𝚌𝚝𝚒𝚗𝚐 𝚠𝚒𝚝𝚑 𝚜𝚎𝚛𝚟𝚎𝚛 𝚙𝚕𝚎𝚊𝚜𝚎 𝚠𝚊𝚒𝚝•• " 
+sleep 0.45
+clear
+echo -e "  𝚌𝚘𝚗𝚗𝚎𝚌𝚝𝚒𝚗𝚐 𝚠𝚒𝚝𝚑 𝚜𝚎𝚛𝚟𝚎𝚛 𝚙𝚕𝚎𝚊𝚜𝚎 𝚠𝚊𝚒𝚝••• " 
+sleep 0.45
+clear
+echo -e "  𝚌𝚘𝚗𝚗𝚎𝚌𝚝𝚒𝚗𝚐 𝚠𝚒𝚝𝚑 𝚜𝚎𝚛𝚟𝚎𝚛 𝚙𝚕𝚎𝚊𝚜𝚎 𝚠𝚊𝚒𝚝•••• " 
+sleep 0.45
+clear
+echo -e "  𝚌𝚘𝚗𝚗𝚎𝚌𝚝𝚒𝚗𝚐 𝚠𝚒𝚝𝚑 𝚜𝚎𝚛𝚟𝚎𝚛 𝚙𝚕𝚎𝚊𝚜𝚎 𝚠𝚊𝚒𝚝• " 
+sleep 0.45
+clear
+echo -e "  𝚌𝚘𝚗𝚗𝚎𝚌𝚝𝚒𝚗𝚐 𝚠𝚒𝚝𝚑 𝚜𝚎𝚛𝚟𝚎𝚛 𝚙𝚕𝚎𝚊𝚜𝚎 𝚠𝚊𝚒𝚝•• " 
+sleep 0.45
+clear
+echo -e "  𝚌𝚘𝚗𝚗𝚎𝚌𝚝𝚒𝚗𝚐 𝚠𝚒𝚝𝚑 𝚜𝚎𝚛𝚟𝚎𝚛 𝚙𝚕𝚎𝚊𝚜𝚎 𝚠𝚊𝚒𝚝••• " 
+sleep 0.45
+clear
+echo -e "  𝚌𝚘𝚗𝚗𝚎𝚌𝚝𝚒𝚗𝚐 𝚠𝚒𝚝𝚑 𝚜𝚎𝚛𝚟𝚎𝚛 𝚙𝚕𝚎𝚊𝚜𝚎 𝚠𝚊𝚒𝚝•••• " 
+sleep 0.45
+clear
+echo -e "  𝚌𝚘𝚗𝚗𝚎𝚌𝚝𝚒𝚗𝚐 𝚠𝚒𝚝𝚑 𝚜𝚎𝚛𝚟𝚎𝚛 𝚙𝚕𝚎𝚊𝚜𝚎 𝚠𝚊𝚒𝚝• " 
+sleep 0.45
+clear
+
+echo -e "𝚂𝙴𝚁𝚅𝙴𝚁 𝙲𝙾𝙽𝙽𝙴𝙲𝚃𝙴𝙳 , 𝙻𝙴𝚃𝚂 𝙴𝙽𝙹𝙾𝚈" | lolcat | pv -qL 220
+sleep 1
+
+
+clear && echo " " && echo " "
+if [ -d /data/data/com.duoqshsqx ]; then
+echo -e "[𝙺𝚁𝙰𝚃𝙾𝚂𝙴𝚇𝙲𝙴𝙻𝙴𝙽𝚃 𝙶𝙶]         𝙵𝙾𝚄𝙽𝙳!" | lolcat | pv -qL 120
+else
+    echo -e "[𝙺𝚁𝙰𝚃𝙾𝚂𝙴𝚇𝙲𝙴𝙻𝙴𝙽𝚃 𝙶𝙶 ] ➟   𝚗𝚘𝚝 𝚏𝚘𝚞𝚗𝚍, 𝚢𝚘𝚞 𝚊𝚛𝚎 𝚗𝚘𝚝 𝚞𝚜𝚒𝚗𝚐 𝙺𝚁𝙰𝚃𝙾𝚂𝙴𝚇𝙲𝙴𝙻𝙴𝙽𝚃 𝙶𝙶" | lolcat | pv -qL 120
+   echo -e ""
+ exit 0
+fi
+fi
+if [ -d /data/data/com.tencent.mobileqqsa ]; then
+echo -e "[𝙺𝙴 𝚅𝙰𝚁𝚃𝚄𝙰𝙻]    𝙵𝙾𝚄𝙽𝙳 !" | lolcat | pv -qL 120
+else
+    echo -e "[𝙺𝙴 𝚅𝙰𝚁𝚃𝚄𝙰𝙻 ] ➟  𝚗𝚘𝚝 𝚏𝚘𝚞𝚗𝚍, 𝚢𝚘𝚞 𝚊𝚛𝚎 𝚗𝚘𝚝 𝚞𝚜𝚒𝚗𝚐 𝙺𝙴 𝚅𝙰𝚁𝚃𝚄𝙰𝙻 " | lolcat | pv -qL 120
+    echo -e ""
+exit 0
+fi
+if ls /sdcard/*/KE BYPASS.lua> /dev/null 2>&1; then
+    echo -e "[𝙺𝙴 𝙱𝚈𝙿𝙰𝚂𝚂  𝚂𝙲𝚁𝙸𝙿𝚃]        𝙵𝙾𝚄𝙽𝙳!" | lolcat | pv -qL 120
+else
+if ls /sdcard/KE BYPASS.lua> /dev/null 2>&1; then
+    echo -e "[𝙺𝙴 𝙱𝚈𝙿𝙰𝚂𝚂  𝚂𝙲𝚁𝙸𝙿𝚃        𝙵𝙾𝚄𝙽𝙳!" | lolcat | pv -qL 120
+ else
+ if ls /sdcard/*/*/KE BYPASS.lua> /dev/null 2>&1; then
+    echo -e "[𝙺𝙴 𝙱𝚈𝙿𝙰𝚂𝚂  𝚂𝙲𝚁𝙸𝙿𝚃]        𝙵𝙾𝚄𝙽𝙳!" | lolcat | pv -qL 120
+ else
+ if ls /sdcard/*/*/*/KE BYPASS.lua> /dev/null 2>&1; then
+    echo -e "[𝙺𝙴 𝙱𝚈𝙿𝙰𝚂𝚂  𝚂𝙲𝚁𝙸𝙿𝚃]        𝙵𝙾𝚄𝙽𝙳!" | lolcat | pv -qL 120
+ else
+    echo -e "[𝙺𝙴 𝙱𝚈𝙿𝙰𝚂𝚂  𝚂𝙲𝚁𝙸𝙿𝚃 ] ➟   𝚗𝚘𝚝 𝚏𝚘𝚞𝚗𝚍, 𝚢𝚘𝚞 𝚊𝚛𝚎 𝚗𝚘𝚝 𝚞𝚜𝚒𝚗𝚐 𝙺𝙴 𝙱𝚈𝙿𝙰𝚂𝚂  𝚂𝙲𝚁𝙸𝙿𝚃" | lolcat | pv -qL 120
+    echo -e ""
+exit 0
+fi
+fi
+fi
+fi
+
+
+if ls /sdcard/*/KE VIP HACK SCRIPT.lua> /dev/null 2>&1; then
+    echo -e "[𝙷𝙰𝙲𝙺 𝚂𝙲𝚁𝙸𝙿𝚃]          𝙵𝙾𝚄𝙽𝙳!" | lolcat | pv -qL 120
+else
+     if ls /sdcard/KE VIP HACK SCRIPT.lua> /dev/null 2>&1; then
+    echo -e "[𝙷𝙰𝙲𝙺 𝚂𝙲𝚁𝙸𝙿𝚃]        𝙵𝙾𝚄𝙽𝙳!" | lolcat | pv -qL 120
+ else
+ if ls /sdcard/*/*/KE VIP HACK SCRIPT.lua> /dev/null 2>&1; then
+    echo -e "[𝙷𝙰𝙲𝙺 𝚂𝙲𝚁𝙸𝙿𝚃]        𝙵𝙾𝚄𝙽𝙳!" | lolcat | pv -qL 120
+ else
+ if ls /sdcard/*/*/*/KE VIP HACK SCRIPT.lua> /dev/null 2>&1; then
+    echo -e "[𝙷𝙰𝙲𝙺 𝚂𝙲𝚁𝙸𝙿𝚃]        𝙵𝙾𝚄𝙽𝙳!" | lolcat | pv -qL 120
+ else
+    echo -e "[𝙷𝙰𝙲𝙺 𝚂𝙲𝚁𝙸𝙿𝚃 ] ➟  𝚗𝚘𝚝 𝚏𝚘𝚞𝚗𝚍, 𝚢𝚘𝚞 𝚊𝚛𝚎 𝚗𝚘𝚝 𝚞𝚜𝚒𝚗𝚐  𝙺𝙴 𝚅𝙸𝙿 𝙷𝙰𝙲𝙺 𝚂𝙲𝚁𝙸𝙿𝚃!" | lolcat | pv -qL 120
+    echo -e ""
+exit 0
+fi
+fi
+fi
+fi
+   echo
+clear
+echo "                                                                             𝐇𝐀𝐂𝐊 𝐃𝐀𝐓𝐀 𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃𝐈𝐍𝐆...            "
+echo -n "  █" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" | lolcat | pv -qL 120
+ if [ -d /storage/emulated/0/KRATOSEXCELENT ]; then
+rm -rf /storage/emulated/0/KRATOSEXCELENT &> /dev/null 
+mkdr /storage/emulated/0/KRATOSEXCELENT &> /dev/null 
+else
+mkdr /storage/emulated/0/KRATOSEXCELENT &> /dev/null 
+wget -O /sdcard/KRATOSEXCELENT/core_patch_1.1.0.14555.pak https://drive.google.com/file/d/1cWCLyrIUX7noxU94XpU0bF3bYNScNEpP/view?usp=drivesdk &> /dev/null 
+wget -O /sdcard/KRATOSEXCELENT/game_patch_1.1.0.14556.pak https://drive.google.com/file/d/1cYfyu1y6_XJWDjNUS9SFN4-Shvv7FAM3/view?usp=drivesdk &> /dev/null 
+wget -O /sdcard/KRATOSEXCELENT/game_patch_1.1.0.14557.pak https://drive.google.com/file/d/1cYQ7hSnYi27x_3obFasMPmwKJRCMZdxW/view?usp=drivesdk &> /dev/null 
+   fi
+   
+   
+sleep 1
+
+clear
+echo -e " ╔═════════════════════════════ ≪ •❈• ≫ ═══════════════════════════════╗
+
+ █░▄▀ █▀▀█ █▀▀█ ▀▀█▀▀ █▀▀▀█ █▀▀▀█   █▀▀▀ ▀▄▒▄▀ █▀▀█ █▀▀▀ █░░░ █▀▀▀ █▄░▒█ ▀▀█▀▀
+ █▀▄░ █▄▄▀ █▄▄█ ░▒█░░ █░░▒█ ▀▀▀▄▄   █▀▀▀ ░▒█░░ █░░░ █▀▀▀ █░░░ █▀▀▀ █▒█▒█ ░▒█░░
+ █░▒█ █░▒█ █░▒█ ░▒█░░ █▄▄▄█ █▄▄▄█   █▄▄▄ ▄▀▒▀▄ █▄▄█ █▄▄▄ █▄▄█ █▄▄▄ █░░▀█ ░▒█░░
+ 
+ ╚═════════════════════════════ ≪ •❈• ≫ ═══════════════════════════════╝" | lolcat -a -d 20
+echo " "
+echo " "
+echo " "
+echo -e "[ 𝚂𝙴𝙻𝙴𝙲𝚃 𝚈𝙾𝚄𝚁 𝙶𝙰𝙼𝙴 ]" | lolcat
